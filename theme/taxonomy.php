ff9f1d1bb845cd0823d6b685f0b2fc238eedd03b
oyic <oyic@outlook.com>
2025-05-01 00:26:15 +0800

    update again

--- theme/taxonomy.php
+++ theme/taxonomy.php
@@ -8,14 +8,18 @@
  */
 
 get_header();
+
 ?>
 
-	<section id="primary">
+
+<section id="primary">
 		<main id="main">
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header flex justify-center items-center">
-				<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+				<h1 class="page-title">
+					Resources: <?php echo get_queried_object()->name;?>
+				</h1>
 			</header><!-- .page-header -->
 			<div class="resources auto-rows-fr grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto">
 				<?php
