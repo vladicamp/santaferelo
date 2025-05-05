b32faddfaf9340f970244001e5613fad7aad84e2
oyic <oyic@outlook.com>
2025-05-05 19:00:15 +0800

    front page and acf new

---
+++ theme/front-page.php
@@ -0,0 +1,28 @@
+<?php
+get_header();
+?>
+
+	<section id="primary">
+		<main id="main">
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				
+				
+
+				// If comments are open, or we have at least one comment, load
+				// the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
