2005a85495c679f3c851807d45565d91da750b8c
oyic <oyic@outlook.com>
2025-05-05 21:12:28 +0800

    aos

--- theme/archive-resources.php
+++ theme/archive-resources.php
@@ -22,7 +22,8 @@ get_header();
 				// Start the Loop.
 				while ( have_posts() ) :
 				the_post();?>
-				<div class="resource card tilt shadow-2xl flex flex-col justify-between items-stretch relative">
+				<div data-aos="flip-up" data-aos-easing="linear"
+				data-aos-duration="3000"  data-aos-delay="300" class="resource card tilt shadow-2xl flex flex-col justify-between items-stretch relative">
 					<div class="card-image relative">
 						<?php if(get_field('lite')):?>
 							<div class="badge badge-primary absolute uppercase font-bold bg-gray-700 text-white px-2 py-1 rounded-sm top-2 right-2">Lite</div>
