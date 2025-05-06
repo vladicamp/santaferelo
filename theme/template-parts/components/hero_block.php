6c238816bbb69ed55519183e2f2cd6553302a9c5
vladimir campos <vladimircampos81@gmail.com>
2025-05-06 18:41:59 +0800

    change image block background

--- theme/template-parts/components/hero_block.php
+++ theme/template-parts/components/hero_block.php
@@ -1,6 +1,6 @@
-<div class="hero-block has-background bg-cover bg-center" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+<div class="hero-block has-background bg-cover bg-center min-h-[60vh]" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
     <div class="max-w-6xl mx-auto py-20 " >
-        <div class="text max-w-2xl prose prose-xl">
+        <div class="text max-w-2xl prose prose-xl bg-black/[.5] p-10">
            <?php the_sub_field('text'); ?>
         </div>
     </div>
